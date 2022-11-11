6edf89d755400f0201dc203880f0259960c0ac2a
ThanhTuanDp04 <115678046+ThanhTuanDp04@users.noreply.github.com>
2022-11-12 00:37:49 +0700

    Updates

--- store/actions/WhoWeAre/index.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import {DPWHOWEARE} from "../../../api/urls";
-
-export const getListDataTeam = createAsyncThunk('DataTeam/getDataTeam', async () => {
-  const response = await axios.get(DPWHOWEARE);
-  return response.data;
-});
--- store/actions/joinus/index.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { DPJOINUS } from "../../../api/urls";
-
-export const getListJoinUs = createAsyncThunk('DataTeam/getDataTeam', async () => {
-    const response = await axios.get(DPJOINUS);
-    return response.data;
-  });
