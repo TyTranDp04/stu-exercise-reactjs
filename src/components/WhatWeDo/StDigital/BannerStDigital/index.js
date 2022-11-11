c4ca6a3200d948106a4d3c2acd102d370441f491
TyTranDp04 <ty.tran_dp04@devplus.edu.vn>
2022-11-11 10:14:18 +0700

    Add Title banner

--- src/components/WhatWeDo/StDigital/BannerStDigital/index.js
+++ src/components/WhatWeDo/StDigital/BannerStDigital/index.js
@@ -1,14 +1,14 @@
 import banner from '../../../../assets/images/bannerstdigital.jpg'
 import { BannerCaption, BannerContainer, BannerDesc, BannerInner, BannerInnerBg, BannerTitle, BannerWrapper } from '../../StSoftware/BannerStSoftware/style'
 
-const BannerStDigital = ({ bannerDigitalDesc, bannerDigitalTitle }) => {
+const BannerStDigital = ({ bannerDigitalDesc, stDigitalTitle }) => {
   return (
     <BannerContainer className='container-fluid'>
       <BannerWrapper className='row'>
         <BannerInner style={{ backgroundImage: `url(${banner})` }}>
           <BannerInnerBg></BannerInnerBg>
           <BannerCaption>
-            <BannerTitle>{bannerDigitalTitle}</BannerTitle>
+            <BannerTitle>{stDigitalTitle}</BannerTitle>
             <BannerDesc>{bannerDigitalDesc}</BannerDesc>
           </BannerCaption>
         </BannerInner>
