c23a94848cc9a57853301d86dfa2d10b2495512e
huynhphuong <phuong.huynh_dp04@devplus.edu.vn>
2022-11-11 17:14:10 +0700

    phuong/#2-fix-home-slide

--- src/components/home/homeSlide/index.js
+++ src/components/home/homeSlide/index.js
@@ -1,5 +1,5 @@
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, {  useEffect } from 'react';
 import { getListDataSlide } from '../../../store/actions/home';
 import Slider from "react-slick";
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,47 +8,7 @@ import {
   Container, 
   ImageCard, 
 } from "./style";
-
-
-
-
-const HomeSlide = () => {
-  const slickSlides = document.querySelectorAll('.slick-slide')
-   const cardFooterRef = document.querySelector('.slide__title-container')
-  // const slickDotsTag = document.querySelector('.slick-dots')
-  // const slickDotTag = slickDotsTag?slickDotsTag.children:''
-  const [current, setCurrent] = useState(0)
-
-  // Object.values(slickSlides).map((e, index)=>{
-
-  //   if(e.className === 'slick-slide slick-active slick-current'){
-  //     console.log(e)
-  //     if(current !== index){
-  //       handleAnimation ()
-  //       setCurrent(index)
-  //       console.log('ok')
-  //     }
-  //   }else{
-
-  //   }
-  // })
-  
- 
-  function handleAnimation (){
-    cardFooterRef.style.display = 'none';
-    setTimeout(()=>{
-      cardFooterRef.style.display = 'block';
-    },100)
-  }
- 
-
-  // console.log(cardFooterRef)
-  // Object.values(slickDotTag).map((e)=>{
-  //   e.onclick = ()=>{
-  //     handleAnimation ()
-  //   }
-  // })
-  
+const HomeSlide = () => { 
   const dispatch = useDispatch()
   const dataHomeSlide = useSelector(state => state.dataHomeSlide.dataHomeSlideState)
   const data = dataHomeSlide.data
